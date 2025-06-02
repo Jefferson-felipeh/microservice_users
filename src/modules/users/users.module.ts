1e8b65c852f77c272180614483e45c4da35ee2f4
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-06-02 16:42:12 -0300

    Criando as configurações do ClientsModule e especificando o broker a ser utlizado na aplicação

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,10 +4,31 @@ import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 import { Users } from "./entities/Users.entity";
 import { UsersRepository } from "./repositories/users.repository";
+import { ClientsModule, Transport } from "@nestjs/microservices";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Users])
+        TypeOrmModule.forFeature([Users]),//Especificando a entidade principal desse módulo de usuários;
+
+        ClientsModule.register([
+            {
+                //Name do clientProxy_
+                name: 'NOTIFICATIONS_SERVICE',
+                //Transport_
+                transport: Transport.RMQ,
+                //options
+                options: {
+                    //Url do endereço do broker robbitmq_
+                    urls: ['amqp://guest:guest@rabbitmq:5672'],
+                    //Nome da fila no broker_
+                    queue: 'notifications_queue',
+                    queueOptions: {
+                        durable: true,
+                    }
+                },
+
+            }
+        ])
     ],
     controllers: [UsersController],
     providers: [UsersService, UsersRepository]
