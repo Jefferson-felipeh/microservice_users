98745cf165e4b1878061f28e8d682082542852dc
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-03 17:03:37 -0300

    Adicionando permissão ao cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4400',
+  });
+
+
   //Receptor que irá receber as chamadas e conexões de outros microservices_
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
