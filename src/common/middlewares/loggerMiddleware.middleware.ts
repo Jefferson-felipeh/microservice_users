04f8286c8685eee4130c6042be5d96b30fba2c91
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-07 17:01:53 -0300

    Middleware para verificar o header

--- src/common/middlewares/loggerMiddleware.middleware.ts
+++ src/common/middlewares/loggerMiddleware.middleware.ts
@@ -1,8 +1,11 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction } from "express";
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware{
-    use(req: any, res: any, next: (error?: any) => void) {
+    use(req: any, res: any, next: NextFunction) {
         //Aqui dentro é onde construimos a lógica de dados do middleware_
+        console.log(req?.headers?.authorization);
+        next();
     }
 }
