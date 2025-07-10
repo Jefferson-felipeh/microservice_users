d9fe25f259edc5850c4f6d5532c9f9d13507d4cc
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-10 16:57:25 -0300

    Modificando a forma como os tokens são extraidos da requisição

--- src/modules/users/strategys/jwtStrategy.strategy.ts
+++ src/modules/users/strategys/jwtStrategy.strategy.ts
@@ -1,12 +1,19 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy, ExtractJwt} from 'passport-jwt';
+import { Request } from 'express';
 
 @Injectable()
 export class JWTStrategy extends PassportStrategy(Strategy){
     constructor(){
         super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            //Extraindo o token do header da requisição_
+            // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+
+            //Extraindo o token dos cookies_
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                (req:Request) => req?.cookies?.accessToken
+            ]),
             ignoreExpiration: false,
             secretOrKey:'jeffersons',
         });
@@ -15,7 +22,6 @@ export class JWTStrategy extends PassportStrategy(Strategy){
     //Estratégia JWT adicionada aos endpoints para autorização de usuários atraves de um token válido fornecido_
     async validate(payload:{user:string,sub:string, exp}){
         if(!payload.user || !payload.sub) throw new UnauthorizedException();
-        
         // const expirationInSeconds = payload.exp;
         // const currentInSeconds = Math.floor(Date.now() / 1000);
 
@@ -26,3 +32,5 @@ export class JWTStrategy extends PassportStrategy(Strategy){
         return payload;
     }
 }
+
+//Eu irei extrair os tokens dos próprios cookies por meio da requisição aos endpoints, assim como estava sendo feito pelo headers da requisição;
